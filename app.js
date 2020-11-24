3dbb9ff5e57d9c7fc172f847070b4b1d3928bc8f
Rohan Gupta <rohan@simplifimed.com>
2020-11-24 13:52:43 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, this is Rohan using AWS DevOps'));
+app.get('/', (req, res) => res.send('Hello World!, CodeBuild and CodePipeline created and tested'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
